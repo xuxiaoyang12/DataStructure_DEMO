68ecd18d9b73694d4c19167a2e7efe3a2eb0bef9
xuxiaoyang <Aa123456789>
2017-09-11 14:01:05 +0800

    '添加arraylist的基础方法'

--- src/main/java/com/xuxy/dataStructure/ArrayList.java
+++ src/main/java/com/xuxy/dataStructure/ArrayList.java
@@ -1,7 +1,6 @@
 package com.xuxy.dataStructure;
 
 
-
 /**
  * Created by Intellij IDEA
  *
@@ -11,29 +10,21 @@ package com.xuxy.dataStructure;
  */
 public class ArrayList {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("实现arrayList集合");
-    //测试arraylist
-        java.util.List lsits  = new java.util.ArrayList();
+        //测试arraylist
 
-       List list = new com.xuxy.dataStructure.arrayList.ArrayList();
-       StringBuffer stringBuffer = new StringBuffer("daxiao");
-       list.add(stringBuffer);
-       list.add(stringBuffer);
-       list.add(stringBuffer);
-       list.add(stringBuffer);
-       list.add(stringBuffer);
-       list.add(stringBuffer);
-       list.add(stringBuffer);
-       list.add(stringBuffer);
-       list.add(stringBuffer);
-       list.add(stringBuffer);
-       list.add(stringBuffer);
-       list.add(stringBuffer);
-       list.add(stringBuffer);
-      for(int i = 0 ; i < list.size() ; i++){
-          System.out.println(list.get(i));
-      }
-      System.out.println(list.size());
+        List list = new com.xuxy.dataStructure.arrayList.ArrayList();
+        String str = "测试数据";
+        //遍历元素 添加默认测试数据
+        for (int i = 0; i < 13; i++) {
+            list.add(str + i);
+        }
+        //移除指定下标元素
+        list.remove(1);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+        System.out.println("集合数组中元素的数量：" + list.size());
     }
 }
--- src/main/java/com/xuxy/dataStructure/arrayList/ArrayList.java
+++ src/main/java/com/xuxy/dataStructure/arrayList/ArrayList.java
@@ -41,18 +41,30 @@ public class ArrayList implements List {
     }
 
     public Object remove(int index) {
+        Object removeElement = null;
         //删除数据  判断是否需要往前移动数组
         if(index < size-1){
             int j = 0;
+            //创建一个新的数组
             Object[] objects = new Object[elementDate.length];
+            //遍历数组将没有删除的元素存放到新的数组中去
             for(int i = 0; i < elementDate.length; i++){
-
+                //判断要删除的下标元素
                 if(index == i){
-                    j--;
+                    removeElement = elementDate[j];
+                    j++;
                 }
-                objects[i] = elementDate[j++];
+                if(j>= elementDate.length){
+
+                }else{
+                    objects[i] = elementDate[j++];
+                }
+
 
             }
+            size--;
+            //新数组赋值到全局中
+            elementDate = objects;
         }else if(index > size-1){
             try {
                 throw new Exception("不存在此索引值");
@@ -60,7 +72,7 @@ public class ArrayList implements List {
                 e.printStackTrace();
             }
         }
-        return null;
+        return removeElement;
     }
 
     public int size() {
@@ -68,6 +80,7 @@ public class ArrayList implements List {
     }
 
     public void set(int index, Object o) {
-
+        //通过下标更新元素值
+        elementDate[index] = o;
     }
 }
